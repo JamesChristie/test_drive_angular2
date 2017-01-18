e55065831262290932479e42f92e68190b441a8b
James Christie <james.aaron.christie@gmail.com>
2017-01-18 12:01:57 -0600

    Implement basic TDD login form

--- src/components/login-form/login-form.component.spec.ts
+++ src/components/login-form/login-form.component.spec.ts
@@ -21,4 +21,90 @@ describe('Component: LoginFormComponent', () => {
   it('should have a defined component', () => {
     expect(component).toBeDefined();
   });
+
+  it('is initialized with a blank sign in model', () => {
+    component.ngOnInit();
+    expect(component.signIn).toEqual({ username: '', password: '' });
+  })
+
+  it('has a blank payload when submitted with empty fields', () => {
+    component.ngOnInit();
+    component.handleSubmit();
+
+    expect(component.payload).toEqual('');
+  });
+
+  it('displays an error when submitted with empty fields', () => {
+    component.ngOnInit();
+    component.handleSubmit();
+
+    expect(component.errorMessage).toEqual(LoginFormComponent.BLANK_ALL_ERROR);
+  });
+
+  it('has a blank payload when submitted with only a username', () => {
+    component.ngOnInit();
+    component.signIn.username = 'username@example.com';
+    component.handleSubmit();
+
+    expect(component.payload).toEqual('');
+  });
+
+  it('displays an error when submitted with only a username', () => {
+    component.ngOnInit();
+    component.signIn.username = 'username@example.com';
+    component.handleSubmit();
+
+    expect(component.errorMessage)
+      .toEqual(LoginFormComponent.BLANK_PASSWORD_ERROR);
+  });
+
+  it('has a blank payload when submitted with only a password', () => {
+    component.ngOnInit();
+    component.signIn.password = 'hunter2';
+    component.handleSubmit();
+
+    expect(component.payload).toEqual('');
+  });
+
+  it('displays an error when submitted with only a password', () => {
+    component.ngOnInit();
+    component.signIn.password = 'hunter2';
+    component.handleSubmit();
+
+    expect(component.errorMessage)
+      .toEqual(LoginFormComponent.BLANK_USERNAME_ERROR);
+  });
+
+  it('has a populated payload when submitted with all fields', () => {
+    component.ngOnInit();
+    component.signIn.username = 'username@example.com';
+    component.signIn.password = 'hunter2';
+    component.handleSubmit();
+
+    expect(component.payload).toEqual(
+      "{\"username\":\"username@example.com\",\"password\":\"hunter2\"}"
+    );
+  });
+
+  it('displays no error message when submitted with all fields', () => {
+    component.ngOnInit();
+    component.signIn.username = 'username@example.com';
+    component.signIn.password = 'hunter2';
+    component.handleSubmit();
+
+    expect(component.errorMessage).not.toBeDefined();
+  });
+
+  it('blanks the error message with a valid submission', () => {
+    component.ngOnInit();
+    component.signIn.username = 'username@example.com';
+    component.handleSubmit();
+
+    expect(component.errorMessage).toBeDefined();
+
+    component.signIn.password = 'hunter2';
+    component.handleSubmit();
+
+    expect(component.errorMessage).not.toBeDefined();
+  })
 });
--- src/components/login-form/login-form.component.ts
+++ src/components/login-form/login-form.component.ts
@@ -1,9 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SignIn } from "../../models/sign-in.model";
 
 @Component({
   selector: 'login-form',
-  template: ''
+  template: require('./login-form.component.html')
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+  static BLANK_ALL_ERROR: string = "Please enter your username and password";
+  static BLANK_USERNAME_ERROR: string = "Please enter your username";
+  static BLANK_PASSWORD_ERROR: string = "Please enter your password";
+
+  signIn: SignIn;
+  payload: string;
+  errorMessage: string;
+
+  ngOnInit() {
+    this.signIn = { username: '', password: '' };
+    this.payload = '';
+  }
+
+  handleSubmit() {
+    this.validate();
+
+    if (!this.errorMessage) {
+      this.payload = JSON.stringify(this.signIn);
+    }
+  }
+
+  private validate() {
+    if (!this.signIn.username && !this.signIn.password) {
+      this.errorMessage = LoginFormComponent.BLANK_ALL_ERROR;
+    } else if (!this.signIn.username) {
+      this.errorMessage = LoginFormComponent.BLANK_USERNAME_ERROR;
+    } else if (!this.signIn.password) {
+      this.errorMessage = LoginFormComponent.BLANK_PASSWORD_ERROR;
+    } else {
+      this.errorMessage = undefined;
+    }
+  }
 }
